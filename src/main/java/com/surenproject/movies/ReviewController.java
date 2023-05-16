ef14ab500dbc8cf01b035862a7c59171d34087a6
suren27k <surendar_new@hotmail.com>
2023-05-16 19:00:39 +0530

    Added access origin for controllers for client

--- src/main/java/com/surenproject/movies/ReviewController.java
+++ src/main/java/com/surenproject/movies/ReviewController.java
@@ -3,14 +3,12 @@ package com.surenproject.movies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1/movies")   //create a better endpoint at /review
 public class ReviewController
 {
